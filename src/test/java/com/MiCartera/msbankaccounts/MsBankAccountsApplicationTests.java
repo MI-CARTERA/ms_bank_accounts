b82e262cbe458f56f8af50c78341f023a56b54b2
Santiago Arocha <arochas846@gmail.com>
2026-04-23 23:15:45 -0300

    Setting things up with generator

---
+++ src/test/java/com/MiCartera/msbankaccounts/MsBankAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MiCartera.msbankaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBankAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
